14f660400d37d70d6b015f9c79a0516b5216f91a
Harsh <harshsharma2210>
2020-05-06 21:58:40 +0530

    FIles Updated

--- app.js
+++ app.js
@@ -15,15 +15,49 @@ app.get("/",function (req,res) {
   });
 
 
+
+
 app.post("/",function (req,res) {
      let firstName=req.body.fName;
      let lastName=req.body.lName;
      let email=req.body.email;
-    console.log(firstName,lastName,email);
-     
+
+     let data={
+       members:[
+         {email_address:email,
+          status:"subscribed",
+          merge_fields:{
+            FNAME:firstName,
+            LNAME:lastName
+          }
+        }
+       ]
+     };
+     let jsonData=JSON.stringify(data);
+     let options={
+       url:"https://us8.api.mailchimp.com/3.0/lists/2ea76020fd",
+        method:"POST",
+        headers:{
+          "Authorization":"HarshS 49bc24c3f4fa8dfbf2dcbda0a26f7ed2-us8"
+        },
+        body:jsonData
+      };
+     request(options,function (error,response,body){
+          if(error) res.sendFile(__dirname+"/failure.html");
+          else  {if(response.statusCode===200){res.sendFile(__dirname+"/success.html");}
+          else {res.sendFile(__dirname+"/failure.html");}
+     }});
   });
 
 
-app.listen(3000,function() {
-    console.log("3000 port");
+  app.post("/failure",function (req,res) {
+    res.redirect("/");
+    });
+ 
+app.listen(process.env.PORT || 3000,function() {
+    console.log("Server is Running");
 });
+
+
+//2ea76020fd
+//49bc24c3f4fa8dfbf2dcbda0a26f7ed2-us8
