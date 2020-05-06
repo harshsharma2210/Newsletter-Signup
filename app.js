c793d3d114c2a8b1558706f211df1b01ac600298
Harsh <harshsharma2210>
2020-05-06 09:27:54 +0530

    Files Updated

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ const request=require("request");
 
 const app=express();
 
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/",function (req,res) {
+
+    res.sendFile(__dirname+"/signup.html");
+  });
+
+
+app.post("/",function (req,res) {
+     let firstName=req.body.fName;
+     let lastName=req.body.lName;
+     let email=req.body.email;
+    console.log(firstName,lastName,email);
+     
+  });
+
+
 app.listen(3000,function() {
     console.log("3000 port");
 });
